94e7aff585501fd580f1bf294cdaa98cc22b58fc
Karpov Mikhail <karpovprog@yandex.ru>
2017-03-08 22:40:11 +0400

    Include localStorage and add methods addTask

--- src/app/homework/tasksList/tasksList.component.ts
+++ src/app/homework/tasksList/tasksList.component.ts
@@ -11,20 +11,33 @@ export class tasksListComponent implements OnInit{
     public taskReady = false;
 
     constructor() {
-        //this.tasks = JSON.parse(localStorage.getItem('tasks'));
+        this.tasks = JSON.parse(localStorage.getItem('tasks'));
+        if (this.tasks == null)
+        {
+            this.tasks = [];
+        }
     }
 
     ngOnInit() {}
 
     public readyChange(task) {
         task.ready = !task.ready;
+        this.saveTasks();
     }
 
     public addTask() {
-
+        this.tasks.push({name: this.taskName, ready: this.taskReady});
+        this.taskName = '';
+        this.taskReady = false;
+        this.saveTasks();
     }
 
     public removeTask(task) {
         this.tasks.splice(this.tasks.indexOf(task), 1);
+        this.saveTasks();
+    }
+
+    public saveTasks() {
+        localStorage.setItem('tasks', JSON.stringify(this.tasks));
     }
 }
